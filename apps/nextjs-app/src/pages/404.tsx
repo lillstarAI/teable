797b35b3da642c228daabbea0d40ff29f1e55d1e
LillstarAI <149671209+lillstarAI@users.noreply.github.com>
2024-07-24 02:58:24 +0200

    Update 404.tsx
    Signed-off-by: LillstarAI <149671209+lillstarAI@users.noreply.github.com>

--- apps/nextjs-app/src/pages/404.tsx
+++ apps/nextjs-app/src/pages/404.tsx
@@ -3,7 +3,7 @@ import { systemConfig } from '@/features/i18n/system.config';
 import { NotFoundPage } from '@/features/system/pages';
 import { getServerSideTranslations } from '@/lib/i18n';
 
-export const getStaticProps = async (context: GetStaticPropsContext) => {
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
   const { locale = 'en' } = context;
 
   const inlinedTranslation = await getServerSideTranslations(locale, systemConfig.i18nNamespaces);
@@ -15,7 +15,6 @@ export const getStaticProps = async (context: GetStaticPropsContext) => {
     },
   };
 };
-
 export default function Custom404(_props: InferGetStaticPropsType<typeof getStaticProps>) {
   return <NotFoundPage />;
 }
