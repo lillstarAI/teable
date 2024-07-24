c2c7ec74ac56da818e5343ce426c50674042aef1
LillstarAI <149671209+lillstarAI@users.noreply.github.com>
2024-07-24 21:24:16 +0000

    tillbaka

--- apps/nextjs-app/src/lib/i18n/index.ts
+++ apps/nextjs-app/src/lib/i18n/index.ts
@@ -1,3 +1,3 @@
 export { getServerSideTranslations } from './getServerSideTranslations';
+export { getTranslationsProps, CookieLocaleKey } from './getTranslationsProps';
 export type { I18nActiveNamespaces, I18nNamespace } from './I18nNamespace.types';
-export * from './getTranslationsProps';
