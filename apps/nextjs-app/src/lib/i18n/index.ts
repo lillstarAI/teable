9042e2fd82ca2f8d2f051a53c6a2dbc94c01ee4c
LillstarAI <149671209+lillstarAI@users.noreply.github.com>
2024-07-24 07:08:11 +0200

    Update index.ts
    Signed-off-by: LillstarAI <149671209+lillstarAI@users.noreply.github.com>

--- apps/nextjs-app/src/lib/i18n/index.ts
+++ apps/nextjs-app/src/lib/i18n/index.ts
@@ -1,4 +1,3 @@
-// index.ts
 export { getServerSideTranslations } from './getServerSideTranslations';
 export { getClientSideTranslations } from './getClientSideTranslations';
 export type { I18nActiveNamespaces, I18nNamespace } from './I18nNamespace.types';
