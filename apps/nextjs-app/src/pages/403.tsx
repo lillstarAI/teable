f9f0f9573485f85685e31bc4ed4677b063d6d62a
LillstarAI <149671209+lillstarAI@users.noreply.github.com>
2024-07-24 02:58:37 +0200

    Update 403.tsx
    Signed-off-by: LillstarAI <149671209+lillstarAI@users.noreply.github.com>

--- apps/nextjs-app/src/pages/403.tsx
+++ apps/nextjs-app/src/pages/403.tsx
@@ -3,7 +3,7 @@ import { systemConfig } from '@/features/i18n/system.config';
 import { ForbiddenPage } from '@/features/system/pages';
 import { getServerSideTranslations } from '@/lib/i18n';
 
-export const getStaticProps = async (context: GetStaticPropsContext) => {
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
   const { locale = 'en' } = context;
 
   const inlinedTranslation = await getServerSideTranslations(locale, systemConfig.i18nNamespaces);
