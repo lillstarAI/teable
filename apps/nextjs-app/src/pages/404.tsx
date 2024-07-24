4ad1a29f4f8352ceb92fa4b75c48abd0b3601045
LillstarAI <149671209+lillstarAI@users.noreply.github.com>
2024-07-24 05:51:05 +0200

    Update 404.tsx
    Signed-off-by: LillstarAI <149671209+lillstarAI@users.noreply.github.com>

--- apps/nextjs-app/src/pages/404.tsx
+++ apps/nextjs-app/src/pages/404.tsx
@@ -1,20 +1,21 @@
-import { useTranslation } from 'next-i18next';
+import type { GetStaticPropsContext, InferGetStaticPropsType } from 'next';
 import { systemConfig } from '@/features/i18n/system.config';
-import { PaymentRequiredPage } from '@/features/system/pages'; // or ForbiddenPage or NotFoundPage
-import { useEffect, useState } from 'react';
-import { getClientSideTranslations } from '@/lib/i18n';
+import { NotFoundPage } from '@/features/system/pages';
+import { getServerSideTranslations } from '@/lib/i18n';
 
-export default function Custom404() { // or Custom402 or Custom403
-  const { i18n } = useTranslation(systemConfig.i18nNamespaces);
-  const [translations, setTranslations] = useState({});
+export const getStaticProps = async (context: GetStaticPropsContext) => {
+  const { locale = 'en' } = context;
 
-  useEffect(() => {
-    const loadTranslations = async () => {
-      const clientTranslations = await getClientSideTranslations(i18n.language, systemConfig.i18nNamespaces);
-      setTranslations(clientTranslations);
-    };
-    loadTranslations();
-  }, [i18n.language]);
+  const inlinedTranslation = await getServerSideTranslations(locale, systemConfig.i18nNamespaces);
 
-  return <PaymentRequiredPage {...translations} />; // or ForbiddenPage or NotFoundPage
+  return {
+    props: {
+      locale: locale,
+      ...inlinedTranslation,
+    },
+  };
+};
+
+export default function Custom404(_props: InferGetStaticPropsType<typeof getStaticProps>) {
+  return <NotFoundPage />;
 }
