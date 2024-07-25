7a2aa4f0782f25fe64d8acba6abb6e3e29ccf416
LillstarAI <149671209+lillstarAI@users.noreply.github.com>
2024-07-25 18:53:16 +0200

    Update 402.tsx
    Signed-off-by: LillstarAI <149671209+lillstarAI@users.noreply.github.com>

--- apps/nextjs-app/src/pages/402.tsx
+++ apps/nextjs-app/src/pages/402.tsx
@@ -1,9 +1,9 @@
-import type { GetStaticPropsContext } from 'next';
+import type { GetServerSidePropsContext } from 'next';
 import { systemConfig } from '@/features/i18n/system.config';
 import { PaymentRequiredPage } from '@/features/system/pages';
 import { getServerSideTranslations } from '@/lib/i18n';
 
-export const getStaticProps = async (context: GetStaticPropsContext) => {
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
   const { locale = 'en' } = context;
 
   const inlinedTranslation = await getServerSideTranslations(locale, systemConfig.i18nNamespaces);
