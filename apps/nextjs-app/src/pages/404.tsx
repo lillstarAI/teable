32ed8d6df2a64d9cf222c14bb19c5826801e0d58
LillstarAI <149671209+lillstarAI@users.noreply.github.com>
2024-07-25 19:58:49 +0200

    Update 404.tsx
    Signed-off-by: LillstarAI <149671209+lillstarAI@users.noreply.github.com>

--- apps/nextjs-app/src/pages/404.tsx
+++ apps/nextjs-app/src/pages/404.tsx
@@ -1,7 +1,21 @@
-import { PaymentRequiredPage } from '@/features/system/pages';
+import type { GetStaticPropsContext, InferGetStaticPropsType } from 'next';
+import { systemConfig } from '@/features/i18n/system.config';
+import { NotFoundPage } from '@/features/system/pages';
+import { getServerSideTranslations } from '@/lib/i18n';
 
-const CustomErrorPage = () => <PaymentRequiredPage />;
+export const getStaticProps = async (context: GetStaticPropsContext) => {
+  const { locale = 'en' } = context;
 
-CustomErrorPage.getInitialProps = () => ({});
+  const inlinedTranslation = await getServerSideTranslations(locale, systemConfig.i18nNamespaces);
 
-export default CustomErrorPage;
+  return {
+    props: {
+      locale: locale,
+      ...inlinedTranslation,
+    },
+  };
+};
+
+export default function Custom404(_props: InferGetStaticPropsType<typeof getStaticProps>) {
+  return <NotFoundPage />;
+}
