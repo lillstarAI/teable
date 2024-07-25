e0aa240b8cac0d6abe02bb0190f0b15c9e5eeb0a
LillstarAI <149671209+lillstarAI@users.noreply.github.com>
2024-07-25 19:58:17 +0200

    Update 402.tsx
    Signed-off-by: LillstarAI <149671209+lillstarAI@users.noreply.github.com>

--- apps/nextjs-app/src/pages/402.tsx
+++ apps/nextjs-app/src/pages/402.tsx
@@ -1,7 +1,21 @@
+import type { GetStaticPropsContext } from 'next';
+import { systemConfig } from '@/features/i18n/system.config';
 import { PaymentRequiredPage } from '@/features/system/pages';
+import { getServerSideTranslations } from '@/lib/i18n';
 
-const CustomErrorPage = () => <PaymentRequiredPage />;
+export const getStaticProps = async (context: GetStaticPropsContext) => {
+  const { locale = 'en' } = context;
 
-CustomErrorPage.getInitialProps = () => ({});
+  const inlinedTranslation = await getServerSideTranslations(locale, systemConfig.i18nNamespaces);
 
-export default CustomErrorPage;
+  return {
+    props: {
+      locale: locale,
+      ...inlinedTranslation,
+    },
+  };
+};
+
+export default function Custom402() {
+  return <PaymentRequiredPage />;
+}
